d223a7d380e579507b4e8f3312b45817b4011d28
Peter deHaan <peter@deseloper.com>
2020-11-16 14:51:30 -0800

    Add CLI arg for JSON-vs-CSV

--- cli.js
+++ cli.js
@@ -1,14 +1,34 @@
 #!/usr/bin/env node
 
+const arg = require("arg");
 const { parse } = require("json2csv");
 const lib = require("./lib");
 
-main();
+const args = arg({
+  '--output': String,
+  // Aliases
+  '-o': '--output',
+});
 
-async function main() {
-  const reviewsJson = await lib.getReviews(process.env.ADDON_ID || 2633704);
-  const reviewsCsv = jsonToCsv(reviewsJson);
-  console.log(reviewsCsv);
+main(args['--output'])
+  .catch(err => {
+    console.error(err.message);
+    process.exitCode = 1;
+  });
+
+async function main(output = "json") {
+  let data = await lib.getReviews(process.env.ADDON_ID || 2633704);
+  switch (output.toLowerCase()) {
+    case "json":
+      data = JSON.stringify(data, null, 2);
+      break;
+    case "csv":
+      data = jsonToCsv(data);
+      break;
+    default:
+      throw new Error(`Unknown output format: "${output}". Expected "JSON" or "CSV"`);
+  }
+  console.log(data);
 }
 
 function jsonToCsv(data = {}) {
