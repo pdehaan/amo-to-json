4e780314e96d3a5f544e609d7e40702bc0416ab3
Peter deHaan <peter@deseloper.com>
2020-11-16 16:11:16 -0800

    Better pagination through 'next' links

--- lib.js
+++ lib.js
@@ -2,43 +2,53 @@ const axios = require("axios");
 const { parse } = require("json2csv");
 const sortJson = require("sort-json");
 
+const BASE_URL = "https://addons.mozilla.org/";
+
 const client = axios.create({
-  baseURL: "https://addons.mozilla.org/",
+  baseURL: BASE_URL,
 });
 
 module.exports = {
+  getApiHref,
   getReviews,
   jsonToCsv,
 }
 
-async function getReviews(addon, page = 1) {
-  let reviews = [];
-  let res;
+function getApiHref(addon) {
+  const url = new URL("/api/v4/ratings/rating/", BASE_URL);
+  url.search = new URLSearchParams({
+    addon,
+    page: 1,
+    page_size: 50
+  });
+  return url.href;
+}
+
+async function getReviews(addon) {
+  const reviews = [];
+  let href = getApiHref(addon);
+  // Fetch all reviews, without using recursion...
   do {
-    res = await client.get("/api/v4/ratings/rating/", {
-      params: {
-        page,
-        page_size: 50,
-        addon,
-      },
-    });
-    reviews = reviews.concat(res.data.results);
-    page += 1;
-  } while (res.data.next);
+    const res = await client.get(href);
+    reviews.push(res.data.results);
+    href = res.data.next;
+  } while (href);
 
   return reviews
+    .flat()
+    .filter(review => !review.is_deleted || !review.is_developer_reply)
     .map((review) => {
+      review.reply = review.reply?.body;
+      review.url = new URL(`/firefox/addon/private-relay/reviews/${review.id}/`, BASE_URL).href;
       review.user = review.user.name;
       review.version = review.version.version;
-      review.reply = review.reply?.body;
       delete review.addon;
       delete review.is_deleted;
       delete review.is_developer_reply;
       delete review.is_latest;
       delete review.previous_count;
       return sortJson(review);
-    })
-    .filter((review) => !review.is_deleted || !review.is_developer_reply);
+    });
 }
 
 function jsonToCsv(data = {}) {
